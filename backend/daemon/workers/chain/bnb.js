f322677af9408bf8e8a3b26d97b45e846f942e7d
Erick Cedeno <108556798+loredounipass@users.noreply.github.com>
2026-09-16 01:25:03 +0000

    Pending changes exported from your codespace

---
+++ backend/daemon/workers/chain/bnb.js
@@ -0,0 +1,23 @@
+const appRoot = require('app-root-path')
+const { Worker } = require(`${appRoot}/config/bullmq`)
+const {
+    connectDB,
+    createTransaction,
+    processDeposit,
+    processWithdraw
+} = require('./index')
+
+connectDB.then(() => {
+    new Worker('bnb-transactions', async (job) => {
+        return await createTransaction(job.data)
+    })
+
+    new Worker('bnb-deposits', async (job) => {
+        return await processDeposit(job.data)
+    })
+
+    new Worker('bnb-withdraws', async (job) => {
+        return await processWithdraw(job.data)
+    })
+
+})
